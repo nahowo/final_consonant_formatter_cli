c11eabfdd94eba0498b3e8e4389869fa0d4d59a4
nahowo <nahowo@naver.com>
2026-04-25 17:03:27 +0900

    feat: add eul-reul function

--- src/fc_formatter_cli/cli.py
+++ src/fc_formatter_cli/cli.py
@@ -3,11 +3,16 @@
 # SPDX-License-Identifier: MIT
 
 import typer
-from fc_formatter import attach_iga
+from fc_formatter import attach_iga, attach_eulreul
 
 app = typer.Typer()
 
-@app.command()
-def build(noun: str, verb: str):
+@app.command(name = "subject")
+def build_subject(noun: str, verb: str):
     attaged_noun = attach_iga(noun)
+    typer.echo(f"created sentence: {attaged_noun} {verb}")
+
+@app.command(name = "object")
+def build_object(noun: str, verb: str):
+    attaged_noun = attach_eulreul(noun)
     typer.echo(f"created sentence: {attaged_noun} {verb}")
